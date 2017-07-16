1e559eb7f9bd58dd120cde1b050682ed6b4820d2
Capi Etheriel <barraponto@gmail.com>
2017-07-16 02:17:22 -0300

    feat(app): added home page (with styles)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,11 +8,61 @@
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <main class="home">
+    <div class="wrapper">
+    <h1>A História da <router-link :to="{name: 'Home'}">&sup1;</router-link>rte</h1>
+    <p>
+      é<router-link :to="{name: 'Home'}">&sup2;</router-link>
+      a área<router-link :to="{name: 'Home'}">&sup3;</router-link>
+      das ciências<router-link :to="{name: 'Home'}">&#x2074;</router-link>
+      humanas<router-link :to="{name: 'Home'}">&#x2075;</router-link>
+      em que se<router-link :to="{name: 'Home'}">&#x2076;</router-link>
+      constrói<router-link :to="{name: 'Home'}">&#x2077;</router-link>
+      uma<router-link :to="{name: 'Home'}">&#x2078;</router-link>
+      narrativa<router-link :to="{name: 'Home'}">&#x2079;</router-link>
+      sobre<router-link :to="{name: 'Home'}">&sup1;&#x2070;</router-link>
+      a criação<router-link :to="{name: 'Home'}">&sup1;&sup1;</router-link>
+      de objetos e experiências<router-link :to="{name: 'Home'}">&sup1;&sup2;</router-link>
+      realizados<router-link :to="{name: 'Home'}">&sup1;&sup3;</router-link>,
+      em sua maioria<router-link :to="{name: 'Home'}">&sup1;&#x2074;</router-link>
+      por<router-link :to="{name: 'Home'}">&sup1;&#x2075;</router-link>
+      homens<router-link :to="{name: 'Home'}">&sup1;&#x2076;</router-link>
+      brancos<router-link :to="{name: 'Home'}">&sup1;&#x2077;</router-link>
+      europeus<router-link :to="{name: 'Home'}">&sup1;&#x2078;</router-link>
+      estadounidenses<router-link :to="{name: 'Home'}">&sup1;&#x2079;</router-link>
+      e pintores<router-link :to="{name: 'Home'}">&sup2;&#x2070;</router-link>
+      (alguns, gênios)<router-link :to="{name: 'Home'}">&sup2;&sup1;</router-link>
+      ...<router-link :to="{name: 'Home'}">&sup2;&sup2;</router-link>
+    </p>
+  </div><!-- /.wrapper -->
+  </main>
 </template>
 
 <style>
-  h1 {
-    color: red;
+  main.home {
+    display: flex;
+    height: 100%;
+    background-color: #ed1c24;
+    color: white;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: center;
+    font-family: Times, Times New Roman, Georgia, serif;
+
+    & p {
+      font-size: 2em;
+      margin: 0;
+    }
+
+    & h1 {
+      margin: 0;
+      text-transform: uppercase;
+      text-decoration: underline;
+    }
+
+    & a {
+      text-decoration: none;
+      color: black;
+    }
   }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './styles.css';
 import Vue from 'vue';
 import router from './router';
 
@@ -6,6 +7,6 @@ import App from './components/App.vue';
 /* eslint-disable no-new */
 new Vue({
   router,
-  el: '#app',
+  el: '#root',
   render: (h) => h(App),
 });
