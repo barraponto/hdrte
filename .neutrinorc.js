087a558d977e438cc3829c80450ac4ed92912737
Capi Etheriel <barraponto@gmail.com>
2017-07-15 22:53:15 -0300

    build(cssnext): reintroduce cssnext

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,6 +3,7 @@ module.exports = {
       "neutrino-preset-eslint-google",
       "neutrino-preset-stylelint-standard",
       "neutrino-preset-web",
+      "neutrino-preset-cssnext",
       "neutrino-preset-vue"
     ]
 };
