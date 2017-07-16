0bff8978227bce9aae74cec32e23cd57684b48f0
Capi Etheriel <barraponto@gmail.com>
2017-07-16 04:57:57 -0300

    feat(app): add newhome component

---
+++ src/components/NewHome.vue
@@ -0,0 +1,58 @@
+<script>
+  export default {
+    name: 'NewHome',
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<template>
+  <main class="new-home">
+    <nav>
+      <router-link :to="{ name: 'New Home' }">
+        <div>1. Sobre o projeto </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>2. Resultados </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>3. Tabelas e Metodologia </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>4. Experiência JSON </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>5. Experiência Novas Tags </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>6. Experiência Random _RT History </div>
+      </router-link>
+      <router-link :to="{ name: 'New Home' }">
+        <div>7. Breve Estudo sobre Gombrich </div>
+      </router-link>
+    </nav>
+    <h1>A HISTÓRIA DA _RTE</h1>
+  </main>
+</template>
+
+<style>
+  main.new-home {
+    padding: 1em;
+    font-family: Courier, "Courier New", monospace;
+    display: flex;
+    flex-direction: column;
+
+    & nav {
+      & a {
+        text-decoration: none;
+        text-transform: uppercase;
+      }
+    }
+
+    & h1 {
+      text-align: center;
+      color: #ed1c24;
+    }
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './components/Home.vue';
+import NewHome from './components/NewHome.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/new-home',
+      name: 'New Home',
+      component: NewHome,
     },
   ],
 });
