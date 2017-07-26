3226444f413dd8e3024c28e46df8587742ab0e67
Capi Etheriel <barraponto@gmail.com>
2017-07-26 17:08:20 -0300

    feat(app): fix the html title

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -2,7 +2,11 @@ module.exports = {
     use: [
       "neutrino-preset-eslint-google",
       "neutrino-preset-stylelint-standard",
-      "neutrino-preset-web",
+      ["neutrino-preset-web", {
+        html: {
+          title: "A História da _rte"
+        }
+      }],
       "neutrino-preset-cssnext",
       "neutrino-preset-vue"
     ]
